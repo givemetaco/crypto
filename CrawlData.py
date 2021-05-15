094df12a33174dd6c4c058b9fb30b7cece59eee5
givemetaco <shwan5186@gmail.com>
2021-05-15 13:49:29 +0900

    Now using multiple coin data available

--- CrawlData.py
+++ CrawlData.py
@@ -3,16 +3,53 @@
 import numpy as np
 import pandas as pd
 
-#Data Source
+# Data Source
 import yfinance as yf
 
 # Get Bitcoin data
 # 이전 24시간동안의 데이터를 15분간격으로 가져옴
-data = yf.download(tickers='BTC-USD', period = '24h', interval = '5m')
+#data = yf.download(tickers='BTC-USD', period='24h', interval='5m')
 
-#if you want get the csv file use the code below
-# data.to_csv("CyrptoData.csv")
 
+def get_data(tickers, period, interval):
+    """
+    ==========================
+    About tickers
+    ==========================
+    Bitcoin -> BTC
+    Ethereum -> ETH
+    Binanace Coin -> BNB
+    Dogecoin -> DOGE
+    Cardano -> ADA
+    Tether ->  USDT
+    XRP -> XRP
+    Polkadot -> DOT
+    Internet Computer -> ICP
+    Bitcoin Cash -> BCH
+    ==========================
+    About period & interval
+    ==========================
+    https://medium.com/analytics-vidhya/python-how-to-get-bitcoin-data-in-real-time-less-than-1-second-lag-38772da43740
+    """
+    try:
+        data = yf.download(tickers=tickers, period=period, interval=interval)
+        return data
+    except:
+        print("error in get data")
+
+
+def create_csv(data):
+    """"This function generates a csv file with cryptocurrency data's"""
+    data.to_csv("CryptoData.csv")
+
+
+def get_datatime(data):
+    """This fuction returs a DatetimeIndex"""
+    return data.index
+
+
+def get_open(data):
+    return data['Open']
 
 # 모든 Datatime 데이터를 가져오는법
 # print(data.index)
---
+++ GraphPlot.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import CrawlData as cd
+x = np.linspace(0, 2, 100)
+
+# Note that even in the OO-style, we use `.pyplot.figure` to create the figure.
+fig, ax = plt.subplots()  # Create a figure and an axes.
+btc_data=cd.get_data('ETH-USD','24h','5m')
+ax.plot(cd.get_datatime(btc_data),cd.get_open(btc_data),label='Bit-Coin')
+
+# ax.plot(x, x, label='linear')  # Plot some data on the axes.
+# ax.plot(x, x**2, label='quadratic')  # Plot more data on the axes...
+# ax.plot(x, x**3, label='cubic')  # ... and some more.
+# ax.set_xlabel('x label')  # Add an x-label to the axes.
+# ax.set_ylabel('y label')  # Add a y-label to the axes.
+# ax.set_title("Simple Plot")  # Add a title to the axes.
+# ax.legend()  # Add a legend.
+
+plt.show()
