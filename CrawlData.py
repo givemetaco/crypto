55e7ffad48f1cbf2e3b8c6517ba13b7421f8afe3
givemetaco <shwan5186@gmail.com>
2021-05-15 14:40:42 +0900

    coin class added

--- CrawlData.py
+++ CrawlData.py
@@ -2,6 +2,7 @@
 # Raw Package
 import numpy as np
 import pandas as pd
+from pandas.core.frame import DataFrame
 
 # Data Source
 import yfinance as yf
@@ -10,46 +11,43 @@ import yfinance as yf
 # 이전 24시간동안의 데이터를 15분간격으로 가져옴
 #data = yf.download(tickers='BTC-USD', period='24h', interval='5m')
 
+class Coin:
+    def __init__(self,tickers,period,interval) -> DataFrame:
+        """
+        ==========================
+        About tickers
+        ==========================
+        Bitcoin -> BTC
+        Ethereum -> ETH
+        Binanace Coin -> BNB
+        Dogecoin -> DOGE
+        Cardano -> ADA
+        Tether ->  USDT
+        XRP -> XRP
+        Polkadot -> DOT
+        Internet Computer -> ICP
+        Bitcoin Cash -> BCH
+        ==========================
+        About period & interval
+        ==========================
+        https://medium.com/analytics-vidhya/python-how-to-get-bitcoin-data-in-real-time-less-than-1-second-lag-38772da43740
+        """
+        try:
+            self.data = yf.download(tickers=tickers, period=period, interval=interval)
+        except:
+            print("error in fetching data")
+        
+        self.datetime=self.data.index
+        self.open_data=self.data['Open']
+        self.high_data=self.data['High']
+        self.low_data=self.data['Low']
+        self.close_data=self.data['Close']
+        self.volume_data=self.data['Volume']
 
-def get_data(tickers, period, interval):
-    """
-    ==========================
-    About tickers
-    ==========================
-    Bitcoin -> BTC
-    Ethereum -> ETH
-    Binanace Coin -> BNB
-    Dogecoin -> DOGE
-    Cardano -> ADA
-    Tether ->  USDT
-    XRP -> XRP
-    Polkadot -> DOT
-    Internet Computer -> ICP
-    Bitcoin Cash -> BCH
-    ==========================
-    About period & interval
-    ==========================
-    https://medium.com/analytics-vidhya/python-how-to-get-bitcoin-data-in-real-time-less-than-1-second-lag-38772da43740
-    """
-    try:
-        data = yf.download(tickers=tickers, period=period, interval=interval)
-        return data
-    except:
-        print("error in get data")
+    def create_csv(self):
+        """"This function generates a csv file with cryptocurrency data's"""
+        self.data.to_csv("CryptoData.csv")
 
-
-def create_csv(data):
-    """"This function generates a csv file with cryptocurrency data's"""
-    data.to_csv("CryptoData.csv")
-
-
-def get_datatime(data):
-    """This fuction returs a DatetimeIndex"""
-    return data.index
-
-
-def get_open(data):
-    return data['Open']
 
 # 모든 Datatime 데이터를 가져오는법
 # print(data.index)
